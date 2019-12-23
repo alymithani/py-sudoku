bf18803d03fd61199d909e45279f7ae407cff0f6
alymithani <mithani5358@gmail.com>
2019-12-23 14:11:23 -0500

    Update sudoku.py

--- code/sudoku.py
+++ code/sudoku.py
@@ -1,10 +1,12 @@
 def print_board(board):
+    # Prints 9x9 array of integers representing the sudoku board
     for i in range(9):
         for j in range(9):
             print(board[i][j], end = ' ')
         print()
 
 def is_complete(board):
+    # Returns true if there are no blank spaces left on the board
     for i in range(9):
         for j in range(9):
             if board[i][j] == 0:
@@ -12,24 +14,28 @@ def is_complete(board):
     return True
 
 def next_blank(board):
+    # returns the index of the next blank space on the board
     for i in range(9):
             for j in range(9):
                 if board[i][j] == 0:
                     return i, j
 
 def valid_row(board, i, val):
+    # returns true if a given value can be placed on a given row on the board
     for j in range(9):
         if board[i][j] == val:
             return False
     return True
 
 def valid_column(board, j, val):
+    # returns true if a given value can be placed on a given column on the board
     for i in range(9):
         if board[i][j] == val:
             return False
     return True
 
 def valid_box(board, i, j, val):
+    # returns true if a given value can be placed in a 3x3 box on the board
     x = (i//3)*3
     y = (j//3)*3
     for i in range (x, x+3):
@@ -39,9 +45,11 @@ def valid_box(board, i, j, val):
     return True
 
 def valid_val(board, i, j, val):
+    # returns true if a given value can be placed on the board given its row, column and 3x3 box
     return valid_row(board, i, val) and valid_column(board, j, val) and valid_box(board, i, j, val)
 
 def solve(board):
+    # returns true if there exists a solution to the given board and completes it
     if is_complete(board):
         return True
     i, j = next_blank(board)
@@ -54,6 +62,7 @@ def solve(board):
     return False
 
 def open_file(file):
+    # returns a 9x9 array of integers representing a sudoku board from a given input file
     f = open(file)
     board = []
     for i in range(9):
