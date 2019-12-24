7bb8db49bb6e658fc3cd98cb6dcc578de4afc0c3
alymithani <mithani5358@gmail.com>
2019-12-24 00:05:47 -0500

    Update board_gen.py

--- code/board_gen.py
+++ code/board_gen.py
@@ -1,3 +1,5 @@
+import sudoku
+
 def open_file(file):
     # returns a 9x9 array of integers representing a sudoku board from a given input file
     f = open(file)
@@ -9,3 +11,24 @@ def open_file(file):
             board[i][j] = int(board[i][j])
     return board
 
+def reflect_x(board):
+    for i in range(5):
+        for j in range(9):
+            temp = board[i][j]
+            board[i][j] = board[8-i][j]
+            board[8-i][j] = temp
+
+def reflect_y(board):
+    for i in range(9):
+        for j in range(5):
+            temp = board[i][j]
+            board[i][j] = board[i][8-j]
+            board[i][8-j] = temp
+
+def rotate(board):
+    # rotate clockwise 90 degrees
+
+if __name__ == "__main__":
+    board = open_file("./seeds/easy.txt")
+    reflect_y(board)
+    sudoku.print_board(board)
